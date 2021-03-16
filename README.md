9d2bef1c61869622b6977f00d056dfe0830c9c3b
ConorWilliams <conorwilliams@outlook.com>
2021-03-16 15:53:17 +0000

    benchmarking

--- README.md
+++ README.md
@@ -4,6 +4,19 @@ A Light, fast, work-stealing thread-pool for C++20. Built on the [`riften::Thief
 
 ## Usage
 
+```C++
 
+#include "riften/thiefpool.hpp"
+
+// Create thread pool with 4 worker threads.
+riften::Thiefpool pool(4);
+
+// Enqueue and store future.
+auto result = pool.enqueue([](int x) { return x; }, 42);
+
+// Get result from future.
+std::cout << result.get() << std::endl;
+
+```
 
 ## Installation
--- include/riften/thiefpool.hpp
+++ include/riften/thiefpool.hpp
@@ -2,11 +2,14 @@
 
 #include <atomic>
 #include <cassert>
+#include <concepts>
 #include <cstdint>
 #include <functional>
 #include <future>
+#include <iostream>  //temp
 #include <ratio>
 #include <thread>
+#include <type_traits>
 #include <utility>
 
 #include "function2/function2.hpp"
@@ -18,7 +21,7 @@ namespace riften {
 namespace detail {
 
 // Bind F and args... into nullary lambda
-template <typename F, typename... Args> auto bind(F &&f, Args &&...arg) {
+template <typename... Args, std::invocable<Args...> F> auto bind(F &&f, Args &&...arg) {
     return [f = std::forward<F>(f), ... arg = std::forward<Args>(arg)]() mutable -> decltype(auto) {
         return std::invoke(std::forward<F>(f), std::forward<Args>(arg)...);
     };
@@ -56,30 +59,33 @@ class Thiefpool {
     explicit Thiefpool(std::size_t threads = std::thread::hardware_concurrency()) : _deques(threads) {
         for (std::size_t i = 0; i < threads; ++i) {
             _threads.emplace_back([&, id = i](std::stop_token tok) {
-                while (!tok.stop_requested() || _in_flight.load(std::memory_order_acquire) > 0) {
+                do {
                     // Wait to be signalled
-                    _deques[id].sem.acquire();
+                    // std::cout << "sleep\n";
 
-                    auto try_do_task_at = [&](std::size_t j) {
-                        if (std::optional one_shot = _deques[j].tasks.steal()) {
-                            // https://www.boost.org/doc/libs/1_75_0/doc/html/atomic/usage_examples.html
-                            if (_in_flight.fetch_sub(1, std::memory_order_release) == 1) {
-                                std::atomic_thread_fence(std::memory_order_acquire);
+                    _deques[id].sem.acquire_all();
+
+                    // std::cout << "wake\n";
+
+                    do {
+                        // Try and do the work just sent to this deque
+                        for (std::size_t i = 0; i < 1'000; i++) {
+                            if (std::optional one_shot = _deques[id].tasks.steal()) {
+                                fetch_sub();
+                                std::invoke(std::move(*one_shot));
                             }
-                            std::invoke(std::move(*one_shot));
                         }
-                    };
+                        // Try and steal some work
+                        for (std::size_t i = id + 1; i < id + _deques.size(); i++) {
+                            if (std::optional one_shot = _deques[i % _deques.size()].tasks.steal()) {
+                                fetch_sub();
+                                std::invoke(std::move(*one_shot));
+                            }
+                        }
 
-                    // Work until the task are done
-                    while (_in_flight.load(std::memory_order_acquire) != 0) {
-                        for (size_t i = 0; i < 10000; i++) {
-                            try_do_task_at(id);
-                        }
-                        for (size_t i = 1; i < _deques.size(); i++) {
-                            try_do_task_at((i + id) % _deques.size());
-                        }
-                    }
-                }
+                    } while (_in_flight.load(std::memory_order_acquire) > 0);
+
+                } while (!tok.stop_requested());
             });
         }
     }
@@ -93,15 +99,15 @@ class Thiefpool {
         }
     }
 
-    template <typename F, typename... Args> auto enqueue(F &&f, Args &&...args) {
+    template <typename... Args, std::invocable<Args...> F> auto enqueue(F &&f, Args &&...args) {
         //
         auto task = detail::NullaryOneShot(detail::bind(std::forward<F>(f), std::forward<Args>(args)...));
 
         auto future = task.get_future();
 
-        _deques[count % _deques.size()].tasks.emplace(std::move(task));
-
         _in_flight.fetch_add(1, std::memory_order_relaxed);
+
+        _deques[count % _deques.size()].tasks.emplace(std::move(task));
 
         _deques[count % _deques.size()].sem.release();
 
@@ -120,6 +126,16 @@ class Thiefpool {
     std::size_t count = 0;
     std::vector<NamesPair> _deques;
     std::vector<std::jthread> _threads;
+
+    // Returns true if did last task
+    bool fetch_sub() {
+        // https://www.boost.org/doc/libs/1_75_0/doc/html/atomic/usage_examples.html
+        if (_in_flight.fetch_sub(1, std::memory_order_release) == 1) {
+            std::atomic_thread_fence(std::memory_order_acquire);
+            return true;
+        }
+        return false;
+    }
 };
 
 }  // namespace riften
